cf91213ae2b7e37d94c16165f401a8b24ca2fa56
Gavinogp18 <115169039+Gavinogp18@users.noreply.github.com>
2024-12-04 15:56:33 -0500

    Update __init__.py

--- mallplaza_app/mallplaza_app/mantenimiento/__init__.py
+++ mallplaza_app/mallplaza_app/mantenimiento/__init__.py
@@ -1,5 +1,7 @@
-from .page import incidencia_page
-
-__all__ = {
-    'incidencia_page',
-}
+from .login import login
+from .empleados import empleados
+from .datos_empleado import datos_empleado
+from .renovar_contrato import renovar_contrato
+from .registrar_empleado import registrar_empleado
+from .eliminar_empleado import eliminar_empleado
+from .desvincular_usuarios import desvincular_empleado
